5c0fdc9c05e39556f8cc13392c7180f15212c856
Dhananjay Kamble <dhanukamble001@gmail.com>
2025-12-27 19:19:28 +0530

    sum of digit

---
+++ LogicBase_solutions/sum of digit.py
@@ -0,0 +1,9 @@
+# Find the sum of the digits of an integer.
+
+num = int(input('Enter number:'))
+sum_of_digit = 0
+
+while num > 0:
+    sum_of_digit += num%10
+    num //= 10
+print(f'Sum of Digit: {sum_of_digit}')
